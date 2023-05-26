2379c88e76474600adf79e0cf60f688e2e160e0f
Niklaus Maurer <niklaus.maurer@rethinc.ch>
2023-05-26 12:00:18 +0200

    Adds CORS configuration to api

--- api/main.py
+++ api/main.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/persons")
