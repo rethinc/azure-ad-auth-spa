e71200866cb2bd436dfe5684650b82bd76d4f8ba
Niklaus Maurer <niklaus.maurer@rethinc.ch>
2023-05-28 10:05:34 +0200

    Adds timeslots endpoint

--- api/main.py
+++ api/main.py
@@ -28,3 +28,29 @@ async def get_persons():
             },
         ]
     }
+
+
+@app.get("/timeslots")
+async def get_persons():
+    return {
+        "data": [
+            {
+                "id": "268f5bbf-58d7-41f9-9cc3-4eac001c5471",
+                "weekday": "Monday",
+                "startTime": "10:30",
+                "endTime": "12:00",
+            },
+            {
+                "id": "f07d38f9-311e-4f3e-8883-dc773d7bbee3",
+                "weekday": "Monday",
+                "startTime": "13:30",
+                "endTime": "15:00",
+            },
+            {
+                "id": "9a0ee5c9-5342-4276-b42e-b4df4a1aa619",
+                "weekday": "Tuesday",
+                "startTime": "10:30",
+                "endTime": "12:00",
+            },
+        ]
+    }
