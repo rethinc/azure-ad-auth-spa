ec14729bb61146cd756160a72ba55017b5a0d84a
Niklaus Maurer <niklaus.maurer@rethinc.ch>
2023-05-29 14:16:52 +0200

    Adds access token provider

--- web/src/application/app/AuthenticatedApp.vue
+++ web/src/application/app/AuthenticatedApp.vue
@@ -1,7 +1,24 @@
-<script setup></script>
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+import { provideAccessToken } from '@/application/app/authentication/AccessToken.provider'
+import { useAuthenticationService } from '@/application/app/authentication/AuthenticationService'
+
+const initialized = ref(false)
+
+onMounted(async () => {
+  const authenticationService = await useAuthenticationService()
+  provideAccessToken(await authenticationService.getAccessToken())
+  initialized.value = true
+})
+</script>
 
 <template>
-  <RouterView />
+  <template v-if="initialized">
+    <RouterView />
+  </template>
+  <template v-else>
+    <h3>Loading...</h3>
+  </template>
 </template>
 
 <style lang="scss">
